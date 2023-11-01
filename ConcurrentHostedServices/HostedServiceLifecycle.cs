25804995d32dca264210f86f2d5a7300c448ef49
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-11-01 21:48:36 +0700

    add life cycle service

---
+++ ConcurrentHostedServices/HostedServiceLifecycle.cs
@@ -0,0 +1,40 @@
+﻿namespace ConcurrentHostedServices;
+
+public class HostedServiceLifecycle : IHostedLifecycleService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("StartAsync is running");
+        return Task.CompletedTask;
+    }
+
+    public Task StartedAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("StartedAsync is running");
+        return Task.CompletedTask;
+    }
+
+    public Task StartingAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("StartingAsync is running");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("StopAsync is running");
+        return Task.CompletedTask;
+    }
+
+    public Task StoppedAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("StoppedAsync is running");
+        return Task.CompletedTask;
+    }
+
+    public Task StoppingAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("StoppingAsync is running");
+        return Task.CompletedTask;
+    }
+}
