7c1e8132267b9c47802ef234f19a2f50e5b163d4
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-11-01 21:13:51 +0700

    add hosted services

---
+++ ConcurrentHostedServices/HostedServiceA.cs
@@ -0,0 +1,33 @@
+﻿namespace ConcurrentHostedServices;
+
+public class HostedServiceA : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        for (var i = 1; i < 10; i++)
+        {
+            Console.WriteLine($"{i} sec waiting");
+            await Task.Delay(1000);
+        }
+        Console.WriteLine("HostedServiceA start method called");
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class HostedServiceB : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("HostedServiceB start method called");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
