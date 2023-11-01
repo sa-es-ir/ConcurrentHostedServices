27fd0daf8ea5191126db9e2a6eca14a7f64d79ca
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-11-01 21:49:05 +0700

    update servcie A

--- ConcurrentHostedServices/HostedServiceA.cs
+++ ConcurrentHostedServices/HostedServiceA.cs
@@ -19,17 +19,3 @@ public class HostedServiceA : IHostedService
         throw new NotImplementedException();
     }
 }
-
-public class HostedServiceB : IHostedService
-{
-    public Task StartAsync(CancellationToken cancellationToken)
-    {
-        Console.WriteLine("HostedServiceB start method called");
-        return Task.CompletedTask;
-    }
-
-    public Task StopAsync(CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
-}
