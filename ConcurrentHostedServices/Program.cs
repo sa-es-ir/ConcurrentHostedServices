9af0c672dd417568e7512fa0d64670d1a6c0a3a8
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-11-01 21:49:16 +0700

    add services to DI

--- ConcurrentHostedServices/Program.cs
+++ ConcurrentHostedServices/Program.cs
@@ -2,13 +2,15 @@ using ConcurrentHostedServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHostedService<HostedServiceA>();
-builder.Services.AddHostedService<HostedServiceB>();
+builder.Services.AddHostedService<HostedServiceLifecycle>();
 
-builder.Services.Configure<HostOptions>(x =>
-{
-    x.ServicesStartConcurrently = true;
-});
+//builder.Services.AddHostedService<HostedServiceA>();
+//builder.Services.AddHostedService<HostedServiceB>();
+
+//builder.Services.Configure<HostOptions>(x =>
+//{
+//    x.ServicesStartConcurrently = true;
+//});
 
 var app = builder.Build();
 
