ad21c0808f833f6e7e20f771b890deb599e6baec
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-11-05 13:13:09 +0700

    change logger for lifecycle servcie

--- ConcurrentHostedServices/HostedServiceLifecycle.cs
+++ ConcurrentHostedServices/HostedServiceLifecycle.cs
@@ -2,39 +2,46 @@
 
 public class HostedServiceLifecycle : IHostedLifecycleService
 {
+    private readonly ILogger<HostedServiceLifecycle> _logger;
+
+    public HostedServiceLifecycle(ILogger<HostedServiceLifecycle> logger)
+    {
+        _logger = logger;
+    }
+
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        Console.WriteLine("StartAsync is running");
+        _logger.LogInformation("StartAsync is running");
         return Task.CompletedTask;
     }
 
     public Task StartedAsync(CancellationToken cancellationToken)
     {
-        Console.WriteLine("StartedAsync is running");
+        _logger.LogInformation("StartedAsync is running");
         return Task.CompletedTask;
     }
 
     public Task StartingAsync(CancellationToken cancellationToken)
     {
-        Console.WriteLine("StartingAsync is running");
+        _logger.LogInformation("StartingAsync is running");
         return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        Console.WriteLine("StopAsync is running");
+        _logger.LogInformation("StopAsync is running");
         return Task.CompletedTask;
     }
 
     public Task StoppedAsync(CancellationToken cancellationToken)
     {
-        Console.WriteLine("StoppedAsync is running");
+        _logger.LogInformation("StoppedAsync is running");
         return Task.CompletedTask;
     }
 
     public Task StoppingAsync(CancellationToken cancellationToken)
     {
-        Console.WriteLine("StoppingAsync is running");
+        _logger.LogInformation("StoppingAsync is running");
         return Task.CompletedTask;
     }
 }
