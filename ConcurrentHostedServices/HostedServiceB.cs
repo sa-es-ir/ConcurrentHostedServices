19b83552321745af3fade08ad8ed937aa7b4fdd4
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2023-11-05 13:12:48 +0700

    change logger for service B

--- ConcurrentHostedServices/HostedServiceB.cs
+++ ConcurrentHostedServices/HostedServiceB.cs
@@ -2,14 +2,22 @@
 
 public class HostedServiceB : IHostedService
 {
+    private readonly ILogger<HostedServiceB> _logger;
+
+    public HostedServiceB(ILogger<HostedServiceB> logger)
+    {
+        _logger = logger;
+    }
+
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        Console.WriteLine("HostedServiceB start method called");
+        _logger.LogInformation("HostedServiceB start method called");
         return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation("HostedServiceB stop method called");
+        return Task.CompletedTask;
     }
 }
