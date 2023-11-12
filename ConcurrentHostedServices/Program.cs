b9808d672b83c118126b84169ae10c90fc3eacf9
sa-es-ir <s.esmaeelinejad@gmail.com>
2023-11-12 21:55:24 +0700

    cleanup program.cs

--- ConcurrentHostedServices/Program.cs
+++ ConcurrentHostedServices/Program.cs
@@ -2,15 +2,17 @@ using ConcurrentHostedServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHostedService<HostedServiceLifecycle>();
+builder.Services.AddHostedService<HostedServiceA>();
+builder.Services.AddHostedService<HostedServiceB>();
 
-//builder.Services.AddHostedService<HostedServiceA>();
-//builder.Services.AddHostedService<HostedServiceB>();
+//builder.Services.AddHostedService<HostedServiceLifecycle>();
 
 //builder.Services.Configure<HostOptions>(x =>
 //{
 //    x.ServicesStartConcurrently = true;
+//    x.ServicesStopConcurrently = true;
 //});
+
 
 var app = builder.Build();
 
